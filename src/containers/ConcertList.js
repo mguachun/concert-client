0fcdf4bedc0c2e4f92c88a253051e9c4827e31cf
Melissa Guachun <1212malka@gmail.com>
2021-10-05 22:53:48 -0400

    concert list draft notes

---
+++ src/containers/ConcertList.js
@@ -0,0 +1,19 @@
+
+
+// const ConcertList = (props) => {
+//     return (
+//       <div>
+//         {Object.values(props.concerts).map(Concert =>
+//           <Concert
+//             key={article.id}
+//             article={article}
+//             actions={props.articleActions}
+//           />
+//         )}
+//       </div>
+//     );
+//   };
+  
+//   export default ConcertList;
+  
+  
