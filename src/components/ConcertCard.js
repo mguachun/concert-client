d1d951af4824118ae99f361994cff4ee237678e5
Melissa Guachun <1212malka@gmail.com>
2021-10-05 22:53:21 -0400

    concert card props

---
+++ src/components/ConcertCard.js
@@ -0,0 +1,14 @@
+
+const ConcertCard = props => {
+    return(
+      <div className="concert-card">
+        <h2>{props.artist}</h2>
+        <p>{props.concert_title}</p>
+        <p>{props.venue}</p>
+        <p>{props.date}</p>
+        <p>{props.genre}</p>
+        <p>{props.comment}</p>
+      </div>
+    )
+  }
+  export default ConcertCard;
