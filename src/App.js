db02040993dc47ef137a1a73d6f7445bb6b47796
Melissa Guachun <1212malka@gmail.com>
2021-10-11 18:22:01 -0400

    about and search import

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
 import Home from "./containers/Home";
+import About from "./containers/About";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import ConcertForm from "./components/forms/ConcertForm";
 import ConcertCard from "./components/ConcertCard";
+import Search from "./components/Search";
 
 
 const App = (props) => {
@@ -15,6 +17,10 @@ const App = (props) => {
         <NavBar />
         <Header />
         <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <div>
+        <Search />
+      </div> 
         <div style={{paddingLeft: "900px"}}className="concert-form">
         <ConcertForm />
         </div>
@@ -23,7 +29,7 @@ const App = (props) => {
         </div>
         
 
-  {/* //fetch request here */}
+  {/* //add map to props from Async lab*/}
       </div>
 
     </Router>
