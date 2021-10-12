8b821610dc550067a10552f1921db5a8106909f0
Melissa Guachun <1212malka@gmail.com>
2021-10-12 15:27:55 -0400

    changed imports for app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
+import './App.css';
 import Home from "./containers/Home";
 import About from "./containers/About";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import ConcertForm from "./components/forms/ConcertForm";
 import ConcertCard from "./components/ConcertCard";
-import Search from "./components/Search";
+import { fetchConcerts } from "./actions/fetchConcerts";
 
+class App extends Component {
 
-const App = (props) => {
-  return (
-    <Router>
-      <div className="app">
-        <NavBar />
-        <Header />
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <div>
-        <Search />
-      </div> 
-        <div style={{paddingLeft: "900px"}}className="concert-form">
-        <ConcertForm />
+  constructor(props) {
+    super()
+    this.state = {} 
+  }
+  state = {
+    concerts: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/concerts")
+    .then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      debugger
+      // dispatch({ type: 'ADD_CONCERTS', concerts: responseJSON })
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <NavBar />
+          <Header />
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <div style={{paddingLeft: "900px"}}className="concert-list">
+          <ConcertForm />
+          </div>
+          <div>
+          <ConcertCard />
+          </div>
+          <div></div>
         </div>
-        <div>
-        <ConcertCard />
-        </div>
-        
+      </Router>
+    );
+  }
+}
+// const mapStateToProps = (state) => {
+//   return {
+  // concerts: state.concerts
+  // loading: state.loading
 
-  {/* //add map to props from Async lab*/}
-      </div>
+//     artist: state.concertsReducer.artists,
+//     concert_title: state.concertsReducer.concert_title,
+//     venue: state.concertsReducer.venue,
+//     date: state.concertsReducer.date,
+//     genre: state.concertsReducer.genre,
+//     comment: state.concertsReducer.comment,
+//   }
+// }
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addConcert: (newConcert) => {
+//       dispatch(addConcert(newConcert))
+//     }
+//   };
+// };
 
-    </Router>
-
-  );
-};
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 export default App;
