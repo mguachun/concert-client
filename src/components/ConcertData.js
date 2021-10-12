2c930b4989e8e0656b1fa20c5986ac64709fcb7a
Melissa Guachun <1212malka@gmail.com>
2021-10-12 15:28:16 -0400

    concert data started

---
+++ src/components/ConcertData.js
@@ -0,0 +1,28 @@
+import { Component } from 'react'
+
+class ConcertData extends Component {
+
+    state = {
+        concerts: []
+    }
+
+    
+
+   render() {
+        const { artist, concert_title, venue, date, genre, comment} = this.props
+     //  console.log(this.props)
+    return (
+        <div className="concert-card"  >
+            <h2>{this.props.artist}</h2>
+            <h3>{this.props.concert_title}</h3>
+            <p>{this.props.venue}</p>
+            <p>{this.props.date}</p>
+            <p>{this.props.genre}</p>
+            <p>{this.props.comment}</p>
+
+        </div>
+        )
+    }
+}
+
+export default ConcertData;
