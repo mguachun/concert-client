f14f4c3bbe6927d852f0d1f66de10a6dc31db9c8
Melissa Guachun <1212malka@gmail.com>
2021-10-12 15:54:09 -0400

    undid fetch changes

--- src/actions/fetchConcerts.js
+++ src/actions/fetchConcerts.js
@@ -11,3 +11,12 @@ export const fetchConcerts = () => {
 }
 
 
+
+// export function fetchConcerts() {
+//   return (dispatch) => {
+//     dispatch({ type: "START_ADDING_CONCERTS_REQUEST" });
+//     fetch("http://localhost:3000/concerts")
+//       .then((response) => response.json())
+//       .then((concerts) => dispatch({ type: "ADD_CONCERTS", concerts }));
+//   };
+// }
