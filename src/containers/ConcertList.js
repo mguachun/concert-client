be5cbc54caa027c89073c0fb334c219c845633ad
Melissa Guachun <1212malka@gmail.com>
2021-10-12 20:00:05 -0400

    deleted code

--- src/containers/ConcertList.js
+++ src/containers/ConcertList.js
@@ -10,9 +10,7 @@ class ConcertList extends Component {
         concerts: [],
 
     };
-    // listConcerts = () => {
-    //     return this.props.banks.map((bank) => {})
-    // }
+  
 
     addConcert = (concertData) => {
         this.setState((prevState) => {
@@ -22,35 +20,21 @@ class ConcertList extends Component {
         })
     }
 
-    makeConcertCard() {
-        let concertCards = this.state.concerts
-        if(this.state.search){
-            concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
-        }
-        return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
-        concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
-    }
+
+
+    // makeConcertCard() {
+    //     let concertCards = this.state.concerts
+    //     if(this.state.search){
+    //         concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
+    //     }
+    //     return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
+    //     concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
+    // }
 
   
-
-    handleSearchChange = (e) => {
-        console.log(e.target.value)
-        this.setState({search: e.target.value})
-    }
-
-    render() {
-        return(
-            <div id="concert-list">
-                <div id="search">
-                    <label><strong>Search:</strong></label><br></br>
-                    <input type="text" onChange={this.handleSearchChange} />
-                </div>
-                <ConcertForm addConcert={this.addConcert}/>
-                {this.makeConcertCard}
-            </div>
-        )
-    }
 }
+
+
 export default ConcertList 
 
   
