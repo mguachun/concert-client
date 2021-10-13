28d0f2df66357b089b36b44d73b9f4de961c256e
Melissa Guachun <1212malka@gmail.com>
2021-10-12 20:51:01 -0400

    spacing

--- src/containers/ConcertList.js
+++ src/containers/ConcertList.js
@@ -4,7 +4,6 @@ import ConcertForm from '../components/ConcertForm'
 import ConcertCard from '../components/ConcertCard'
 import Search from '..components/Search'
 import ConcertData from '../components/ConcertData'
-
 class ConcertList extends Component {
     state = {
         concerts: [],
@@ -22,16 +21,33 @@ class ConcertList extends Component {
 
 
 
-    // makeConcertCard() {
-    //     let concertCards = this.state.concerts
-    //     if(this.state.search){
-    //         concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
-    //     }
-    //     return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
-    //     concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
-    // }
+    makeConcertCard() {
+        let concertCards = this.state.concerts
+        if(this.state.search){
+            concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
+        }
+        return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
+        concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
+    }
 
-  
+    handleSearchChange = (e) => {
+        console.log(e.target.value)
+        this.setState({search: e.target.value})
+    }
+
+    render() {
+        return(
+            <div id="concert-list">
+                <div id="search">
+                    <label><strong>Search:</strong></label><br></br>
+                    <input type="text" onChange={this.handleSearchChange} />
+                </div>
+                <ConcertForm addConcert={this.addConcert}/>
+                {this.makeConcertCard}
+            </div>
+        )
+    }
+
 }
 
 
