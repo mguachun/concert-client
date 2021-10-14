ba19abc5264cda27027635b56b8762d577721aab
Melissa Guachun <1212malka@gmail.com>
2021-10-14 15:09:29 -0400

    added componentdidmount and state

--- src/containers/ConcertList.js
+++ src/containers/ConcertList.js
@@ -1,51 +1,79 @@
 import { Component } from 'react'
-import Home from '../containers/Home'
-import ConcertForm from '../components/ConcertForm'
-import ConcertCard from '../components/ConcertCard'
-import Search from '..components/Search'
+import concertsReducer from '../reducers/concertsReducer'
+// import Home from '../containers/Home'
+// import ConcertCard from '../components/ConcertCard'
+// import Search from '..components/Search'
 import ConcertData from '../components/ConcertData'
 class ConcertList extends Component {
-    state = {
-        concerts: [],
+    constructor(props) {
+        super()
+        this.state = {} 
+    }
 
-    };
+    state = {
+        concerts: []
+    }
+    
+    componentDidMount() {
+        fetch("http://localhost:3000/concerts")
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                // debugger
+                // dispatch({ type: 'ADD_CONCERTS', concerts: responseJSON })
+                // console.log(responseJSON)})
+                this.addConcert(data)
+            }
+        )
+    }
+
   
 
     addConcert = (concertData) => {
-        this.setState((prevState) => {
-            return {
-                concerts: [...prevState.concerts, concertData]
-            }
+        // console.log(concertData)
+        this.setState( {
+            // return {
+                concerts: concertData
+                // concerts: [this.set]
+            // }
         })
+        //we want an array of objects
+    
+        // console.log(this.state.concerts)
+    }
+
+    displayConcerts = () => {
+        return(
+        this.state.concerts.map(object => <div><h1>{object.artist}</h1> {object.concert_title}, {object.venue}, {object.date}, {object.genre},{object.comment}</div>)
+        )
+
     }
 
 
 
-    makeConcertCard() {
-        let concertCards = this.state.concerts
-        if(this.state.search){
-            concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
-        }
-        return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
-        concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
-    }
+    // makeConcertCard() {
+    //     let concertCards = this.state.concerts
+    //     if(this.state.search){
+    //         concertCards = this.state.concerts.filter((concert) => concert.title.toLowerCase().includes(this.state.search.toLowerCase()))
+    //     }
+    //     return concertCards.map(concert => <Concert key={concert.id} id={concert.id} artist={concert.artist} 
+    //     concert_title={concert.concert_title} venue={concert.venue} date={concert.date} genre={concert.genre} comment={concert.comment}/>)
+    // }
 
-    handleSearchChange = (e) => {
-        console.log(e.target.value)
-        this.setState({search: e.target.value})
-    }
+
 
     render() {
-        return(
-            <div id="concert-list">
-                <div id="search">
-                    <label><strong>Search:</strong></label><br></br>
-                    <input type="text" onChange={this.handleSearchChange} />
-                </div>
-                <ConcertForm addConcert={this.addConcert}/>
-                {this.makeConcertCard}
+        // debugger
+    //    console.log(this.state.concerts)
+        return (
+            <div>
+                <h1>Concert Index:</h1>
+                { this.state.concerts ? this.displayConcerts() : "loading"}
             </div>
         )
+
+        
+
     }
 
 }
