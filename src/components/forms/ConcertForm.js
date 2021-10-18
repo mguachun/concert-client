62d62a3e9f2f37f6a2abcb2691da88fb35e662cd
Melissa Guachun <1212malka@gmail.com>
2021-10-17 22:53:45 -0400

    changed controlled form

--- src/components/forms/ConcertForm.js
+++ src/components/forms/ConcertForm.js
@@ -37,12 +37,13 @@ class ConcertForm extends Component {
     render() {
         return (
             <div className="form-container">
+                <form>
                 <h1> Track a Ticket </h1>
-                <form onSubmit={this.handleSubmit}>
+                <div>
+                     {/* the value of each input must rely on the state
                     <label>Artist:</label>
-                    {/* the value of each input must rely on the state */}
                     <input type="text" value={this.state.artist} onChange={this.handleChange} name="artist"/> <br />
-                    <br></br>
+                    </div>
                     <label>Concert Title:</label>
                     <input type="text" value={this.state.concert_title} onChange={this.handleChange} name="concert_title"/> <br />
                     <br></br>
@@ -58,10 +59,42 @@ class ConcertForm extends Component {
                     <label>Comment:</label>
                     <input type="comment" value={this.state.comment} onChange={this.handleChange} name="comment"/> <br />
                     <br></br>
-                    <input type="submit" />
+                    <input type="submit" /> */}
+                    <label htmlFor="artist">Artist:</label>
+                    <input className="artist" />
+                    </div>
+                    <br></br>
+                    <div>
+                    <label htmlFor="Concert Title">Concert Title:</label>
+                    <input className="concert-title" />
+                    </div>
+                    <br></br>
+                    <div>
+                    <label htmlFor="Venue">Venue:</label>
+                    <input className="venue" />
+                    </div>
+                    <br></br>
+                    <div>
+                    <label htmlFor="Date">Date:</label>
+                    <input className="date" />
+                    </div>
+                    <br></br>
+                    <div>
+                    <label htmlFor="Genre">Genre:</label>
+                    <input className="genre" />
+                    </div>
+                    <br></br>
+                    <div>
+                    <label htmlFor="Comment">Comment:</label>
+                    <input className="comment" />
+                    </div>
+                    <br></br>
+                    <div>
+                        <button>Submit</button>
+                    </div>
                 </form>
                 <img
-          style={{ float: "left", padding: "10px", paddingLeft: "172px", marginTop: "-338px" }}
+          style={{ float: "left", padding: "10px", paddingLeft: "172px", marginTop: "-350px" }}
           src="https://media.giphy.com/media/MUFlTPzL3fBmwIPiTK/giphy.gif"
         //   "https://media.giphy.com/media/ThfdBssxMqeE8/giphy.gif"
         // "https://media.giphy.com/media/kinlAqef3yqTIj9PyB/giphy.gif"
