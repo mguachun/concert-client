60ffe34e252caaeb53b953e0e387601439106550
Melissa Guachun <1212malka@gmail.com>
2021-10-19 16:27:18 -0400

    styling how to

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import HowTo from "../components/HowTo";
 
 
 const Home = () => {
     return (
-      <div>
+      <div className="ticket">
         <h3 style={{textAlign: "left", paddingLeft: "50px"}}>"<em>Tracking concerts and memories"</em></h3>
         <p
         style={{
@@ -11,24 +12,24 @@ const Home = () => {
           textAlign: "center",
           fontSize: 21,
           padding: "30px",
-          paddingRight: "50px",
+          paddingRight: "20px",
           paddingLeft: "20px",
           borderStyle: "dashed",
           borderRadius: "25px",
           borderWidth: "2px",
           borderColor: "#3621ad",
           marginLeft: "20px",
-          marginRight: "20px"
-    
+          marginRight: "20px",
+  
         }}
       >
         <strong>Ticket Tracker</strong> is a single page application that helps you keep track of 
         your concerts and live shows. It allows you to view concerts you've seen in the past and logs shows you have
-        planned for the future.
-
+        planned for the future. 
+        <span className="how-to"> <HowTo /></span>
       </p>
         <img
-          style={{ float: "left", padding: "10px", paddingLeft: "220px", marginTop: "-260px" }}
+          style={{ float: "left", padding: "10px", paddingLeft: "220px", marginTop: "-340px" }}
           src="https://media.giphy.com/media/MUFlTPzL3fBmwIPiTK/giphy.gif"
           alt="blinking ticket gif"
           height="50px"
