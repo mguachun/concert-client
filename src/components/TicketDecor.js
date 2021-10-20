91daac051c4b0ab03b37ec8597b3a6b97a61b50e
Melissa Guachun <1212malka@gmail.com>
2021-10-20 15:11:53 -0400

    ticket stateless component gif

---
+++ src/components/TicketDecor.js
@@ -0,0 +1,16 @@
+import React from "react";
+const TicketDecor = () => {
+    return(
+    <div>
+        <img
+            style={{ float: "left", padding: "10px", paddingLeft: "220px", marginTop: "-340px" }}
+            src="https://media.giphy.com/media/MUFlTPzL3fBmwIPiTK/giphy.gif"
+            alt="blinking ticket gif"
+            height="50px"
+            width="50px"
+        />
+    </div>
+    )
+
+}
+export default TicketDecor;
