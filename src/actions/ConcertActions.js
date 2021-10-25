94efe83fe6ae5f93bfa7c360aa45690034ac5004
Melissa Guachun <1212malka@gmail.com>
2021-10-25 17:11:22 -0400

    new fetch to update state

--- src/actions/ConcertActions.js
+++ src/actions/ConcertActions.js
@@ -29,4 +29,19 @@ export const createConcert = (concert) => {
     })
   }  
 }
+export const newFetchConcerts = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CONCERTS'})
+    fetch("http://localhost:3000/concerts")
+    .then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      // console.log(responseJSON)
+      dispatch({ type: 'ADD_CONCERTS', concerts: responseJSON })
+    })
+  }
+}
+  
+
+
 
