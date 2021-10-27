d3207c4a6ef89ce40059f3b9c94d7aea1f8de6ff
Melissa Guachun <1212malka@gmail.com>
2021-10-27 18:25:48 -0400

    created concert for container component

---
+++ src/containers/Concert.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class Concert extends Component {
+    render() {
+        
+        return (
+            <div className="concert-data"><h1>{this.props.object.artist} 🎤   </h1>
+                Concert Title: {this.props.object.concert_title}<br />
+                Venue: {this.props.object.venue} <br />
+                Date: {this.props.object.date} <br />
+                Genre: {this.props.object.genre}<br />
+                Comment: <em> {this.props.object.comment} </em>
+                <button className="list-delete-btn" onClick={() => this.props.deleteConcert(this.props.object)}> Delete X</button>
+            </div>
+        )
+ 
+    }
+}
+export default Concert;
