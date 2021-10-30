06fe77d71ce0066b0617625ea80d2da8d29a7ecf
Melissa Guachun <1212malka@gmail.com>
2021-10-30 17:27:17 -0400

    add concert reducer

--- src/reducers/concertsReducer.js
+++ src/reducers/concertsReducer.js
@@ -1,4 +1,3 @@
-// import {Search} from "./ConcertActions"
 const concertsReducer = (state = { concerts: [], loading: false }, action) => {
   switch(action.type) {
     case 'LOADING_CONCERTS':
@@ -7,13 +6,20 @@ const concertsReducer = (state = { concerts: [], loading: false }, action) => {
         concerts: [...state.concerts],
         loading: true
       }
-    case 'ADD_CONCERTS':
-      return {
-        ...state,
-        concerts: action.concerts,
-        loading: false
-      }
-
+    //case 'ADD_CONCERT':
+      // adds one more concert to the concert array (don't mutate the array - create a new one)
+      case 'ADD_CONCERT':
+        return {
+          ...state, 
+          concerts: [state.concerts, action.concert],
+          loading: false
+        }
+      case "SET_CONCERTS":
+        return {
+          ...state,
+          concerts: action.concerts,
+          loading: false
+        }
       case 'DELETE_CONCERT':
         return {
           concerts: state.concerts.filter(concert => concert.id !== action.payload.id),
