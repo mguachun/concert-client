3634eee0a5c348d87b37df6f8dbd744a779422ce
Melissa Guachun <1212malka@gmail.com>
2021-10-30 17:26:20 -0400

    notes deleted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,11 +21,8 @@ class NavBar extends React.Component {
       <div>
         <NavLink 
           to="/"
-          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
           exact
-          /* add styling to Navlink */
           style={link}
-          /* add prop for activeStyle */
           activeStyle={{
             background: '#3621ad'
           }}
