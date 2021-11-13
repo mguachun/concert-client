cc127a908cf3da60036e5fdd7ad0b6ec711dda29
Melissa Guachun <1212malka@gmail.com>
2021-11-13 16:36:19 -0500

    filtered drop down

--- src/containers/ConcertList.js
+++ src/containers/ConcertList.js
@@ -5,20 +5,54 @@ import {deleteConcert} from "../actions/ConcertActions"
 
 class ConcertList extends Component {
 
-    displayConcerts = () => {
+   state = {
+    genre: 'All'
+}
+
+    filteredConcerts = () => {
+        
         return (
-            this.props.concerts.map(instance => <Concert deleteConcert={this.props.deleteConcert} object={instance}/> )
+        this.props.concerts.filter(concert => concert.genre.includes(this.state.genre))
+
+        )
+    }
+
+    handleChange = (e) => {
+        this.setState({genre: e.target.value })
+    }
+
+
+    displayConcerts = () => {
+        const concerts = this.state.genre == "All" ? this.props.concerts : this.filteredConcerts()
+        return (
+            concerts.map(instance => <Concert deleteConcert={this.props.deleteConcert} object={instance}/> )
         )
     }
 
     render() {
         return (
             <div>
+                <label for="search-concerts"> Choose a Genre</label>
+                <select name="genres" id="genre" onChange={this.handleChange} value={this.state.genre}>
+                    <option value="All">All</option>
+                    <option value="Rock">Rock</option>
+                    <option value="Rap">Rap</option>
+                    <option value="Heavy Metal">Heavy Metal</option>
+                    <option value="Alternative Country">Alternative Country</option>
+                    <option value="Indie">Indie</option>
+                    <option value="Experimental Pop">Experimental Pop</option>
+                    <option value="Hip Hop">Hip Hop</option>
+                    <option value="Country">Country</option>
+                    <option value="Punk">Punk</option>
+                    <option value="Pop">Pop</option>
+                    <option value="Classical">Classical</option>
+                </select>
                 
+        
                 <h1 className="c-index">Concert Index:</h1>
                 {!this.props.loading ? this.displayConcerts() : "loading"}
 
-        
+               
             </div>
         )
         
