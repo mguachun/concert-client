0f3a5deaa23c3786546fb0ec8335cdf2dd9a6bb5
Melissa Guachun <1212malka@gmail.com>
2022-04-01 18:02:45 -0400

    classname added

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HowTo = () => {
     return (
-        <div>
-            <p> 🎵 To get started, go to "Add a Ticket" to track a concert and start building your collection! 🎵 </p>
+        <div >
+            <p className="how-to"> 🎵 To get started, go to "Add a Ticket" to track a concert and start building your collection! 🎵 </p>
         </div>
     )
 
