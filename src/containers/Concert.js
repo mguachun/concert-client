6de97c6b1446ccd103bf3c26417aa533302cf0bb
Melissa Guachun <1212malka@gmail.com>
2022-04-01 16:16:02 -0400

    changed orientation of cards for concerts

--- src/containers/Concert.js
+++ src/containers/Concert.js
@@ -4,17 +4,34 @@ class Concert extends Component {
 
     render() {
         return (
-            <div className="concert-data"><h1>{this.props.object.artist} 🎤   </h1>
-                Concert Title: {this.props.object.concert_title}<br />
-                Venue: {this.props.object.venue} <br />
-                Date: {this.props.object.date} <br />
-                Genre: {this.props.object.genre}<br />
-                Comment: <em> {this.props.object.comment} </em>
-                <button className="list-delete-btn" onClick={() => this.props.deleteConcert(this.props.object)}> Delete X</button>
-                
+            <div className="cards" >
+
+                    <div className="cards_item">
+                        <div className="card">
+                            <div className="card_content">
+                                <h1 className="card-title">{this.props.object.artist} 🎤   </h1>
+                                Concert Title: {this.props.object.concert_title}<br />
+                                Venue: {this.props.object.venue} <br />
+                                Date: {this.props.object.date} <br />
+                                Genre: {this.props.object.genre}<br />
+                                Comment: <em> {this.props.object.comment} </em>
+                                <button className="list-delete-btn" onClick={() => this.props.deleteConcert(this.props.object)}> Delete X</button>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+
+
+      
+
+
+
             </div>
+
         )
- 
+
     }
 }
 export default Concert;
