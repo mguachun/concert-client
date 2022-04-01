683aa1ff7a8965aa956693f27ab7a2f2487e1932
Melissa Guachun <1212malka@gmail.com>
2022-04-01 18:03:01 -0400

    changed styling to link and nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,27 +4,32 @@ import { NavLink } from 'react-router-dom';
 const link = {
 
 
-  width: '100px',
-  margin: '0 4px 4px',
+  // width: '100px',
+  float: "right",
+  marginRight: '20px',
+  margin: '0 10px 4px',
   background: 'rgb(66, 68, 125)',
   textDecoration: 'none',
   color: 'white',
-  borderRadius: '30px',
-  border: '1px',
-  padding: '10px',
+  // borderRadius: '30px',
+  // border: '1px',
+
+  padding: '5px',
+
   fontSize: '19px',
 }
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div>
+      <div className='nav'>
         <NavLink 
           to="/"
           exact
           style={link}
           activeStyle={{
-            background: 'rgb(66, 68, 125)'
+            background: 'rgb(66, 68, 125)',
+    
           }}
         >Home</NavLink>
         <NavLink
@@ -40,7 +45,10 @@ class NavBar extends React.Component {
           exact
           style={link}
           activeStyle={{
-            background: 'rgb(66, 68, 125)'
+            background: 'rgb(66, 68, 125)',
+
+         
+
           }}
           >About</NavLink>
       </div>
