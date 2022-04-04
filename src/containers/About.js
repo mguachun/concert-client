edfb071c852198fd930e9d0a5f0b5a16e40bb389
Melissa Guachun <1212malka@gmail.com>
2022-04-04 12:04:37 -0400

    changed gif and styling of about

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,22 +4,20 @@ import React from "react";
 const About = () => {
     return (
       <div>
-        <h3 style={{textAlign: "left", paddingLeft: "50px", fontSize: "25px"}}>About 🎫 :</h3>
+        <h3 style={{textAlign: "left", paddingLeft: "50px", fontSize: "25px"}}>About:</h3>
         <p
         style={{
-          
+          color:"white",
+          backgroundColor: "rgb(66, 68, 125)",
           textAlign: "center",
           fontSize: "21px",
           padding: "60px",
           paddingRight: "50px",
           paddingLeft: "50px",
           marginLeft: "200px",
-          borderStyle: "dashed",
-          borderRadius: "25px",
-          borderWidth: "2px",
-          borderColor: "rgb(34, 3, 31)",
-          width: "700px",
-          height: "600px"
+          width: "800px",
+          height: "750px",
+          borderRadius: "15px"
           
 
         }}
@@ -29,13 +27,12 @@ const About = () => {
        for safe keeping. Every so often I would take the box out to go through my tickets to relive the memories
        I had from each concert. Concerts have changed quite a bit since then but tickets continue to be a momento that encapsulates
        special moments in time. The digitization of tickets and concerts all together is what ultimately 
-       inspired me to bring this concept into a new age. 
+       inspired me to bring this concept into a new age.<br></br>
        <img
-          style={{ float: "center", height: "260px", width: "500px", paddingTop: "50px"}}
-          src="https://media.giphy.com/media/cgW5iwX0e37qg/giphy.gif"
+          style={{ float: "center", height: "400px", width: "500px", paddingTop: "50px", borderRadius: "50%"  }}
+          src="https://media.giphy.com/media/J9dXe6GwvmJmU/giphy.gif"
           alt="Jake from Adventure Time listening to a walkman"
-          height="50px"
-          width="50px"
+        
         />
 
 
